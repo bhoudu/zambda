7e53751929fb5ea04213fbefef7aa9107d203f9e
Benjamin Houdu <benjamin.houdu@gmail.com>
2022-04-19 09:15:07 +0200

    :wrench: fix S3

--- src/s3.ts
+++ src/s3.ts
@@ -1,25 +1,39 @@
 import { Upload } from "@aws-sdk/lib-storage";
-import { S3Client, S3 } from "@aws-sdk/client-s3";
+import { S3, Tag } from "@aws-sdk/client-s3";
 
-try {
-  const parallelUploads3 = new Upload({
-    client: new S3({}) || new S3Client({}),
-    tags: [...], // optional tags
-    queueSize: 4, // optional concurrency configuration
-    partSize: 5MB, // optional size of each part
-    leavePartsOnError: false, // optional manually handle dropped parts
-    params: {
-      Bucket: '',
-      Key: '',
-      Body: '',
-    },
-  });
-
-  parallelUploads3.on("httpUploadProgress", (progress) => {
-    console.log(progress);
-  });
-
-  await parallelUploads3.done();
-} catch (e) {
-  console.log(e);
+export async function push(
+  s3: S3,
+  bucket: string,
+  key: string,
+  body: string | Uint8Array | Buffer,
+  tags: Tag[] = [],
+  queueSize: number = 4,
+  partSize: number = 5,
+): Promise<boolean> {
+  try {
+    const parallelUploads3 = new Upload({
+      client: s3,
+      tags: [...tags], // optional tags
+      queueSize, // optional concurrency configuration
+      partSize, // optional size of each part in MB
+      leavePartsOnError: false, // optional manually handle dropped parts
+      params: {
+        Bucket: bucket,
+        Key: key,
+        Body: body,
+      },
+    });
+    parallelUploads3.on(
+      "httpUploadProgress",
+      (progress) => {
+        console.log(progress);
+      }
+    );
+    return parallelUploads3
+      .done()
+      .then(() => true);
+  } catch (e) {
+    console.log(e);
+    return false;
+  }
 }
