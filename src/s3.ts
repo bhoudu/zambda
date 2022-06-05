53e2014456bbffa1ffcb4aaa5ce92785a63c4199
Benjamin Houdu <benjamin.houdu@gmail.com>
2022-06-05 18:17:46 +0200

    :zap: update S3 function

--- src/s3.ts
+++ src/s3.ts
@@ -1,27 +1,41 @@
 import { Upload } from '@aws-sdk/lib-storage';
 import { S3, Tag } from '@aws-sdk/client-s3';
+import { PutObjectCommandInput } from '@aws-sdk/client-s3/dist-types/commands/PutObjectCommand';
+import { PutObjectRequest } from '@aws-sdk/client-s3/dist-types/models/models_0';
 
-export async function push(
+/**
+ * Push zambda generated zip to S3 bucket
+ *
+ * @param s3 client to use
+ * @param bucket to push zip in
+ * @param key in S3 bucket
+ * @param body zip file
+ * @param tags to put along file in S3
+ * @param queueSize to use with multiple part upload client
+ * @param partSize to use with multiple part upload client
+ */
+export async function pushS3(
   s3: S3,
   bucket: string,
   key: string,
-  body: string | Uint8Array | Buffer,
+  body: PutObjectRequest['Body'] | string | Uint8Array | Buffer,
   tags: Tag[] = [],
-  queueSize = 4,
-  partSize = 5,
+  queueSize = 2,
+  partSize = 1,
 ): Promise<boolean> {
   try {
+    const params: PutObjectCommandInput = {
+      Bucket: bucket,
+      Key: key,
+      Body: body,
+    };
     const parallelUploads3 = new Upload({
       client: s3,
       tags: [...tags], // optional tags
       queueSize, // optional concurrency configuration
       partSize, // optional size of each part in MB
       leavePartsOnError: false, // optional manually handle dropped parts
-      params: {
-        Bucket: bucket,
-        Key: key,
-        Body: body,
-      },
+      params,
     });
     parallelUploads3.on('httpUploadProgress', (progress) => {
       console.log(progress);
